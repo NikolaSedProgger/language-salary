8b5c7c2e90f0f6e496f27d2c6d6ccb7e47503dcb
NikolaSedProgger <forniteturbokosh@gmail.com>
2021-12-18 14:32:18 +0300

    Избавился от сравнений None

--- get_language_salary.py
+++ get_language_salary.py
@@ -1,9 +1,9 @@
 def get_language_salary(salary_from, salary_to):
     average_salary = 0
-    if salary_from == None:
+    if salary_from is None:
         average_salary = salary_to * 0.8
-    elif salary_to == None:
+    elif salary_to is None:
         average_salary = salary_from * 1.2
-    elif salary_from and salary_to == None:
-        average_salary = None
+    elif salary_from and salary_to is None:
+        average_salary = 0
     return average_salary
