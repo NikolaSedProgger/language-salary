cabd81f58be11c6b6fff4c89a3e4846377291265
NikolaSedProgger <forniteturbokosh@gmail.com>
2022-01-15 15:10:56 +0300

    Упростил код

--- get_language_salary.py
+++ get_language_salary.py
@@ -1,9 +1,7 @@
 def get_language_salary(salary_from, salary_to):
-    average_salary = 0
-    if salary_to:
-        average_salary = salary_to * 0.8
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
     elif salary_from:
-        average_salary = salary_from * 1.2
-    else:
-        average_salary = 0
-    return average_salary
+        return salary_from * 1.2
+    elif salary_to:
+        return salary_to * 0.8
